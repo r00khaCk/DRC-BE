c913d2100fce38090afdc85c8b6dbe2d2481d0d5
r00khaCk <deilanraj9799@gmail.com>
2023-06-18 22:53:21 +0800

    database initialization script

---
+++ db_init/init.sql
@@ -0,0 +1,33 @@
+-- Connect to the newly created database
+\c crypthub_db_2;
+
+CREATE SCHEMA IF NOT EXISTS cryptHubSchema;
+
+-- Create the "users" table
+CREATE TABLE cryptHubSchema.users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  password VARCHAR(50)
+);
+
+CREATE TABLE cryptHubSchema.wallet (
+  wallet_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES cryptHubSchema.users(id),
+  currency VARCHAR(50),
+  amount DECIMAL(10, 2)
+);
+
+CREATE TABLE cryptHubSchema.transactions (
+  transaction_id SERIAL PRIMARY KEY,
+  wallet_id INT REFERENCES cryptHubSchema.wallet(wallet_id),
+  user_id INT REFERENCES cryptHubSchema.users(id),
+  transaction_amount DECIMAL(10, 2),
+  transaction_date TIMESTAMP
+);
+
+CREATE TABLE cryptHubSchema.deposit (
+  deposit_id SERIAL PRIMARY KEY,
+  wallet_id INT REFERENCES cryptHubSchema.wallet(wallet_id),
+  deposit_amount DECIMAL(10, 2)
+);
