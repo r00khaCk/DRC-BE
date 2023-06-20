c8ef3cce6202848a71abaae487499ca69a53e227
r00khaCk <deilanraj9799@gmail.com>
2023-06-21 02:28:10 +0800

    check for duplicate email and hashing the passwords from the request body

--- app/api/models/users.js
+++ app/api/models/users.js
@@ -1,26 +1,52 @@
+import bcrypt from "bcrypt";
 import database from "../../services/db.js";
 
 // Register new account
-export async function registerNewUser(registerDetails) {
+export const registerNewUser = async (registerDetails) => {
   const { name, email, password } = registerDetails;
-  let response;
-  //check if email, name and password is true
-  if (name && email && password) {
-    let values = [name, email, password];
-    try {
-      response = await database.connection.query(
-        "INSERT INTO crypthubschema.users (name,email,password) VALUES($1,$2,$3) RETURNING *",
-        values
-      );
-    } catch (error) {
-      console.log("Error in query");
-      console.log(error);
-      throw error;
-    }
-    return response.rows[0];
+  // storedEmail will fetch records where the email equals to the registerDetails email
+  let storedEmail = await database.connection.query(
+    "SELECT * FROM crypthubschema.users WHERE email=$1",
+    [email]
+  );
+  // storedEmail.rows.length returns the length of the rows
+  if (storedEmail.rows.length) {
+    // if storedEmail.rows.length is > 0 means that there is a duplicate email in the db
+    console.log(`${email} exists in the database`);
+    return "DUPLICATE_EMAIL";
   } else {
-    throw new Error("Bad Request");
+    bcrypt.hash(password, 10, async (err, hash) => {
+      if (err) {
+        return "HASHING_ERROR";
+      } else {
+        let hashPassword = hash;
+        let response;
+        // Check if email, name, and password are true
+        if (name && email && hashPassword) {
+          let values = [name, email, hashPassword];
+          try {
+            response = await database.connection.query(
+              "INSERT INTO crypthubschema.users (name,email,password) VALUES($1,$2,$3) RETURNING *",
+              values
+            );
+          } catch (error) {
+            console.log("Error in query");
+            console.log(error);
+            throw error;
+          }
+        } else {
+          throw new Error("Bad Request");
+        }
+      }
+    });
   }
-}
+};
 
-// export default Users;
+//Helper methods
+// const _sendResponse = (res, registerDetails) => {
+//   res.status(200).send(registerDetails || {});
+// };
+
+// const _sendError = (res, status, code) => {
+//   res.status(status).send({ error: code || "UNKNOWN_ERROR" });
+// };
