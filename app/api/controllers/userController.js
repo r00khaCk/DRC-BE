5d553a51f4cfb6490ef03f1a2c4b1594f35876f6
r00khaCk <deilanraj9799@gmail.com>
2023-06-21 02:24:26 +0800

    created error responses for duplicate email and hashing error

--- app/api/controllers/userController.js
+++ app/api/controllers/userController.js
@@ -1,14 +1,25 @@
 import * as UserModel from "../models/users.js";
 
-export async function registerNewUser(req, res) {
+export const registerNewUser = async (req, res) => {
   try {
     let user = await UserModel.registerNewUser(req.body);
-    res.status(201).json({
-      message: "User created",
+    // error responses
+    if (user === "DUPLICATE_EMAIL") {
+      return res.status(400).json({
+        message: "DUPLICATE_EMAIL",
+      });
+    } else if (user === "HASHING_ERROR") {
+      return res.status(500).json({
+        message: "HASHING_ERROR",
+      });
+    }
+    return res.status(201).json({
+      message: "USER_CREATED",
     });
-  } catch (error) {
+    // catches error in the request body, i.e: missing key when post request is sent
+  } catch (err) {
     return res.status(500).json({
-      error: error,
+      error: err,
     });
   }
-}
+};
