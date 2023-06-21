62ec0fe3d8aed1cdd676eb34d984a1150f717545
r00khaCk <deilanraj9799@gmail.com>
2023-06-21 17:47:29 +0800

    created sendVerificationEmail() controller and called it in registerNewUser()

--- app/api/controllers/userController.js
+++ app/api/controllers/userController.js
@@ -1,6 +1,6 @@
 import * as UserModel from "../models/users.js";
 
-export const registerNewUser = async (req, res) => {
+export const registerNewUser = async (req, res, next) => {
   try {
     let user = await UserModel.registerNewUser(req.body);
     // error responses
@@ -13,10 +13,27 @@ export const registerNewUser = async (req, res) => {
         message: "PASSWORD_HASHING_ERROR",
       });
     }
+    if (!sendVerificationEmail(req.body)) {
+      return res.status(500).json({
+        message: "VERIFICATION_EMAIL_ERROR",
+      });
+    }
     return res.status(201).json({
       message: "USER_CREATED",
     });
     // catches error in the request body, i.e: missing key when post request is sent
+  } catch (err) {
+    return res.status(500).json({
+      error: err,
+    });
+  }
+};
+
+const sendVerificationEmail = async (userDetails, res) => {
+  try {
+    UserModel.verifyNewAccount(userDetails, (message) => {
+      // console.log(emailSent, "message");
+    });
   } catch (err) {
     return res.status(500).json({
       error: err,
