e10785e15faa734ee3a6c5a81237dd745405a53e
abdul <Ainzburn@gmail.com>
2023-06-21 09:49:22 +0800

    Installing cookie-parser library

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import * as UserRouter from "./api/routes/userRoute.js";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(json());
+app.use(cookieParser());
 app.use("/user", UserRouter.router);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
