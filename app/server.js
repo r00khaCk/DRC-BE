02cf3ab541a4b4027e820572268912408b025c7b
r00khaCk <deilanraj9799@gmail.com>
2023-06-23 00:18:35 +0800

    added CORS

--- app/server.js
+++ app/server.js
@@ -7,9 +7,9 @@ import morgan from "morgan";
 import * as UserRouter from "./api/routes/userRoute.js";
 import testRouter from "./api/routes/test-route.js";
 
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const app = express();
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,6 +18,23 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./api/views"));
 // app.use(cookieParser());
+
+// middleware that requests will go through and responses that are sent back to the client will have headers appended to it
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  // method gives access to the http method used on the request
+  if (req.method === "OPTIONS") {
+    // methods that may be sent with the request
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use("/user", UserRouter.router);
 
 // app.use("/test", testRouter);
