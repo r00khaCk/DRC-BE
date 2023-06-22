db47914f22c9bc9199d9baef58bb6446c73112b1
r00khaCk <deilanraj9799@gmail.com>
2023-06-22 14:28:27 +0800

    added setup for ejs

--- app/server.js
+++ app/server.js
@@ -1,16 +1,23 @@
 import express, { json } from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 import morgan from "morgan";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import * as UserRouter from "./api/routes/userRoute.js";
 import testRouter from "./api/routes/test-route.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./api/views"));
+// app.use(cookieParser());
 app.use("/user", UserRouter.router);
 
 // app.use("/test", testRouter);
