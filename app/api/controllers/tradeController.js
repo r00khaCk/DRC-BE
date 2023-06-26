6a7bd4cfbcbc969a981a5875874cbfd7e7959b27
r00khaCk <deilanraj9799@gmail.com>
2023-06-26 23:48:40 +0800

    changed naming convention to snake_case for variables

--- app/api/controllers/tradeController.js
+++ app/api/controllers/tradeController.js
@@ -1,8 +1,8 @@
 import * as TradeModel from "../models/trade.js";
 
 export const buyTrade = async (req, res) => {
-  let buyOrder = await TradeModel.buyCoinsModel(req.body, req.headers);
-  if (buyOrder.status === "BUY_ERROR") {
+  let buy_order = await TradeModel.buyCoinsModel(req.body, req.headers);
+  if (buy_order.status === "BUY_ERROR") {
     return res.status(400).json({
       message: "BUY_ORDER_FAILED",
     });
@@ -10,9 +10,9 @@ export const buyTrade = async (req, res) => {
   return res.status(200).json({
     message: "BUY_ORDER_SUCCESS",
     details: {
-      orderAmount: buyOrder.orderAmount,
-      walletBalance: buyOrder.walletBalance,
-      coinCurrency: buyOrder.coinCurrency,
+      orderAmount: buy_order.order_amount,
+      coinCurrency: buy_order.coin_currency,
+      walletBalance: buy_order.wallet_balance,
     },
   });
 };
