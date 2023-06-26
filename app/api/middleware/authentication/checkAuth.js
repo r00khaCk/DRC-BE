77da46abdfa4b7e13d0647633942cf52bb3efb5c
r00khaCk <deilanraj9799@gmail.com>
2023-06-26 11:24:47 +0800

    checkAuth() middleware completed

--- app/api/middleware/authentication/checkAuth.js
+++ app/api/middleware/authentication/checkAuth.js
@@ -5,8 +5,10 @@ const env = process.env;
 export const checkAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    console.log("token from checkAuth: ", token);
     const decoded = jwt.verify(token, env.SECRET_KEY);
-    req.userEmail = decoded;
+    console.log("decoded: ", decoded);
+    req.email = decoded;
     next();
   } catch (error) {
     return res.status(401).json({
