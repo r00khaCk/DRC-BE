4bcfb12ab09d56e8cab11f921afd9b7813052143
r00khaCk <deilanraj9799@gmail.com>
2023-07-04 12:01:26 +0800

    changed addNewP2PContract() to addNewP2PContractModel()

--- app/api/controllers/p2pController.js
+++ app/api/controllers/p2pController.js
@@ -1,7 +1,10 @@
 import * as P2PModel from "../models/p2p.js";
 
 export const addP2PContract = async (req, res) => {
-  let p2p_contract = await P2PModel.addNewP2PContract(req.body, req.headers);
+  let p2p_contract = await P2PModel.addNewP2PContractModel(
+    req.body,
+    req.headers
+  );
   if (p2p_contract.status === "INPUT_QUERY_SUCCESS") {
     res.status(201).json({ message: "CONTRACT_ADDED" });
   } else if (p2p_contract.status === "INPUT_QUERY_FAILURE") {
