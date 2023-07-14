f5886dc49bc9733b555dc0fd4e2677ca6b6b971d
r00khaCk <deilanraj9799@gmail.com>
2023-07-15 00:57:52 +0800

    updated scripts

--- config/initial.sh
+++ config/initial.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# activate the ssh agent 
-eval "$(ssh-agent -s)" && ssh-add /root/.ssh/new_do_id_rsa && ssh-add -l
-git pull origin main
+# pull the updates
+error_message="Please make sure you have the correct access rights and the repository exists."
+success_message="Already up to date."
+
+
+cd DRC-BE/
+echo "In DRC-BE directory"
+eval "$(ssh-agent -s)" && ssh-add ../.ssh/new_do_id_rsa && ssh-add -l &&
+pull_command=$(git pull origin main)
+if echo "$pull_command" | grep -q "$error_message"; then
+        eval "$(ssh-agent -s)" && ssh-add ../.ssh/new_do_id_rsa && ssh-add -l &&
+        $pull_command
+elif echo "$pull_command" | grep -q "$success_message"; then 
+        echo "$pull_command"
+fi
 
 
 # install dependencies
 cd app/ && npm i && cd ..
-
-# stop and remove the containers
-docker compose down --volumes
-
-# remove any unused images 
-# Get the image IDs
-unused_image_ids=$(docker images |grep none| awk '{print $3}')
-# Counter for removed images
-removed=0
-# Loop through the image IDs and remove the images
-for id in $unused_image_ids; do
-    docker rmi $id
-    removed=$((removed + 1))
-    if [ $unused_image_ids -eq 0 ]; then
-        break
-    fi
-done
-echo "Images removed: $removed"
 
 # run the containers
 docker compose up --build
--- config/update.sh
+++ config/update.sh
@@ -1,11 +1,19 @@
+# Script is run when there is an update in the main while the server is running
+
 # down the containers
 docker compose down --volumes &&
 # pull the updates
-pull_command=$(git pull origin main) 
-error_message="Please make sure you have the correct access rights and the repository exists."
-if [ echo $pull_command | grep -q $error_message ]; then
-    eval "$(ssh-agent -s)" && ssh-add /root/.ssh/new_do_id_rsa && ssh-add -l &&
-    $pull_command
-
+pull_command=$(git pull origin main)
+if echo "$pull_command" | grep -q "$error_message"; then
+        eval "$(ssh-agent -s)" && ssh-add ../.ssh/new_do_id_rsa && ssh-add -l &&
+        $pull_command
+elif echo "$pull_command" | grep -q "$success_message"; then 
+        echo "$pull_command"
 fi
 
+
+# install dependencies
+cd app/ && npm i && cd ..
+
+# run the containers
+docker compose up --build
