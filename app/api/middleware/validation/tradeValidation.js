47ea4113bb9ebf98d4c71907dbe538ef58496e1a
r00khaCk <deilanraj9799@gmail.com>
2023-07-19 13:54:16 +0800

    added validation for buy trade

--- app/api/middleware/validation/tradeValidation.js
+++ app/api/middleware/validation/tradeValidation.js
@@ -1,6 +1,23 @@
 import { body } from "express-validator";
 
-export const tradeValidation = [
+export const buyTradeValidation = [
+  body("input_amount")
+    .exists({ checkFalsy: false })
+    .withMessage("Input amount required")
+    .isNumeric()
+    .withMessage("Input amount must be a number")
+    .isFloat({ gt: 0 })
+    .withMessage("Input amount must be greater than 0"),
+  body("coin_amount")
+    .exists({ checkFalsy: false })
+    .withMessage("Coin amount required")
+    .isNumeric()
+    .withMessage("Coin amount should be a number")
+    .isFloat({ gt: 0 })
+    .withMessage("Coin amount should be more than 0"),
+];
+
+export const sellTradeValidation = [
   body("coin_amount")
     .exists({ checkFalsy: false })
     .withMessage("Coin amount required")
